67733cec0528159f4130345d7c706dece6151a60
leeyiding <1843272640@qq.com>
2022-07-19 16:17:10 +0800

    添加网页安全提醒重定向脚本

---
+++ scripts/redirect.js
@@ -0,0 +1,67 @@
+// ==UserScript==
+// @name         去除站外链接安全提示
+// @namespace    https://github.com/leeyiding/AdGuardRules
+// @version      0.1
+// @description  通过重定向去除站外链接安全提示
+// @author       leeyiding
+// @match        https://www.coolapk.com/link*
+// @match        https://www.jianshu.com/go-wild*
+// @match        https://link.csdn.net/*
+// @match        https://link.juejin.cn/*
+// @match        https://link.zhihu.com/*
+// @match        https://c.pc.qq.com/middlem.html*
+// @match        https://wx.mail.qq.com/list/readtemplate*
+// @match        https://mail.qq.com/cgi-bin/readtemplate*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+    const queryURLParams = (variable) => {
+        var query = window.location.search.substring(1);
+        var vars = query.split("&");
+        for (var i = 0; i < vars.length; i++) {
+            var pair = vars[i].split("=");
+            if (pair[0] == variable) { return decodeURIComponent(pair[1]); }
+        }
+        return (false);
+    }
+
+    let urlList = [{
+        url: 'https://www.coolapk.com/link',
+        param: 'url'
+    }, {
+        url: 'https://www.jianshu.com/go-wild',
+        param: 'url'
+    }, {
+        url: 'https://link.csdn.net',
+        param: 'target'
+    }, {
+        url: 'https://link.juejin.cn',
+        param: 'target'
+    }, {
+        url: 'https://link.zhihu.com',
+        param: 'target'
+    }, {
+        url: 'https://c.pc.qq.com/middlem.html',
+        param: 'pfurl'
+    }, {
+        url: 'https://wx.mail.qq.com/list/readtemplate',
+        param: 'gourl'
+    }, {
+        url: 'https://mail.qq.com/cgi-bin/readtemplate',
+        param: 'gourl'
+    }]
+
+    let full_url = window.location.href;
+    urlList.forEach(item => {
+        if (full_url.indexOf(item.url) === 0) {
+            let redirect_url = queryURLParams(item.param);
+            if (!redirect_url) {
+                return;
+            }
+            window.location.href = redirect_url;
+        }
+    })
+})();
+
