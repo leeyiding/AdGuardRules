eec63c6383be68453af2e16896a28dd7532fdb50
leeyiding <1843272640@qq.com>
2022-07-19 19:51:58 +0800

    补全脚本信息，设置脚本运行时机

--- scripts/redirect.js
+++ scripts/redirect.js
@@ -1,9 +1,13 @@
 // ==UserScript==
 // @name         去除站外链接安全提示
 // @namespace    https://github.com/leeyiding/AdGuardRules
-// @version      0.1
+// @version      0.2
 // @description  通过重定向去除站外链接安全提示
 // @author       leeyiding
+// @downloadURL  https://raw.githubusercontent.com/leeyiding/AdGuardRules/main/scripts/redirect.js
+// @updateURL    https://raw.githubusercontent.com/leeyiding/AdGuardRules/main/scripts/redirect.js
+// @homepage     https://github.com/leeyiding/AdGuardRules
+// @supportURL   https://github.com/leeyiding/AdGuardRules/issues
 // @match        https://www.coolapk.com/link*
 // @match        https://www.jianshu.com/go-wild*
 // @match        https://link.csdn.net/*
@@ -13,6 +17,7 @@
 // @match        https://wx.mail.qq.com/list/readtemplate*
 // @match        https://mail.qq.com/cgi-bin/readtemplate*
 // @grant        none
+// @run-at      document-start
 // ==/UserScript==
 
 (function () {
